f4ba9a2ea9ade4f5bf5da8b56ee9a7850aa8fcbd
Kreshsv <kreshkatana29@gmail.com>
2023-08-17 09:46:29 +0530

    Project-Management-System-Nextjs

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ['bcrypt'],
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+}
+
+
+
+module.exports = nextConfig
